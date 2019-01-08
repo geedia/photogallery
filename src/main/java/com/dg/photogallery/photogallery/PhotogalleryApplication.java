60c201af2efbeed713e9c19acc72ddf7ad219080
geedia <geedia@gmail.com>
2019-01-07 22:20:27 -0500

    Update PhotogalleryApplication.java

--- src/main/java/com/dg/photogallery/photogallery/PhotogalleryApplication.java
+++ src/main/java/com/dg/photogallery/photogallery/PhotogalleryApplication.java
@@ -2,11 +2,18 @@ package com.dg.photogallery.photogallery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class PhotogalleryApplication {
+public class PhotogalleryApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotogalleryApplication.class, args);
 	}
+	
+	@Override
+    	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PhotogalleryApplication.class);
+    }
 }
